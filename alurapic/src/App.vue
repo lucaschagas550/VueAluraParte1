abb43dee4a5f1df818f43caaf923d71c19029ab2
Lucas Andrade <lucas.chagas@opuspacind.onmicrosoft.com>
2021-09-09 16:54:53 -0300

    Feat: Mostrar e esconder imagem. Lista com Filtro

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -4,7 +4,7 @@
 
 <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="filtre por parte do titulo">
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotos" v-bind:key="foto.titulo">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro" v-bind:key="foto.titulo">
 
         <meu-painel :titulo="foto.titulo">
           <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
@@ -31,6 +31,21 @@ export default {
       filtro: ''
     };
   },
+
+  computed: {
+    fotosComFiltro(){
+
+      if(this.filtro){
+        /*filtrar*/
+
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      }else{
+        return this.fotos;
+      }
+    }
+  },
+
   created() {
     this.$http
       .get("http://localhost:3000/v1/fotos")
