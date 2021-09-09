5d6588e95e01bed34080bc3e0015bd43c9bb871f
Lucas Andrade <lucas.chagas@opuspacind.onmicrosoft.com>
2021-09-09 16:27:59 -0300

    Feat: Inicio do projeto.

---
+++ alurapic/src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+
+<input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="filtre por parte do titulo">
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotos" v-bind:key="foto.titulo">
+
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
+        </meu-painel>
+
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Painel from './components/shared/painel/Painel';
+
+export default {
+
+  components:{
+    'meu-painel' : Painel
+  },
+
+  data() {
+    return {
+      titulo: "AluraPic",
+      fotos: [],
+      filtro: ''
+    };
+  },
+  created() {
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
+      .then(res => res.json())
+      .then(fotos => (this.fotos = fotos), err => console.log(err));
+  }
+};
+</script>
+
+<style>
+.corpo {
+  font-family: Helvetica, sans-serif;
+  width: 96%;
+  margin: 0 auto;
+}
+
+.centralizado {
+  text-align: center;
+}
+
+.titulo {
+  text-align: center;
+}
+
+.corpo {
+  font-family: Helvetica, sans-serif;
+  margin: 0 auto;
+  width: 96%;
+}
+
+.lista-fotos {
+  list-style: none;
+}
+
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
+
+.imagem-responsiva {
+  width: 100%;
+}
+
+.filtro {
+  display: block;
+  width: 100%;
+}
+
+</style>
